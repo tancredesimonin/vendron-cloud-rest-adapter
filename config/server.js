2c34ba760c687090036521b3f2e6abb866acbc1d
Tancrède Simonin <tancrede.simonin.eco@gmail.com>
2022-01-31 13:17:09 +0100

    build(strapi) first setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '983532ec366d3956a10055e0876e9003'),
+    },
+  },
+});
